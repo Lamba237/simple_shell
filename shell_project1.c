b8ebe5038a700b62d27f4d419033a0f21956fa6a
Lamba237 <simontanyi2004@gmail.com>
2023-12-11 02:31:18 -0800

    UPdates
    removed the print_string function to a solo file called print_String.c

--- shell_project1.c
+++ shell_project1.c
@@ -1,41 +1,28 @@
 #include "main.h"
 /**
- * print_string - writes a string to stdout
- * @str: The string to print
- */
-void print_string(const char *str)
-{
-	ssize_t len = 0;
-
-	while (str[len] != '\0')
-		len++;
-	write(1, str, len);
-}
-
-/**
  * main - entry point for the shell program
  * Return: Always 0
  */
 int main(void)
 {
-	char *buffer = NULL;
-	size_t bufsize = 0;
+	char *lineptr = NULL;
+	size_t size = 0;
 
 	while (1)
 	{
 		print_string("#cisfun$ ");
 
-		if (getline(&buffer, &bufsize, stdin) == -1)
+		if (getline(&lineptr, &size, stdin) == -1)
 		{
 			print_string("\n");
 			break;
 		}
 
-		remove_newline(buffer);
-		execute_command(buffer);
+		remove_newline(lineptr);
+		execute_command(lineptr);
 	}
 
-	free(buffer);
+	free(lineptr);
 
 	return (0);
 }
